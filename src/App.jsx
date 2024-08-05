e92a3e3f9107f92dd8881a150b3e907258c99212
flaudado <flavialaudado@gmail.com>
2024-08-04 22:00:40 -0300

    chore: add commnets

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import ContextApp from "./ContextApp";
 import ZustandApp from "./ZustandApp";
 import ReduxApp from "./ReduxApp";
-
+//agregar practica de spread operators 
 function App() {
   console.log("render app.jsx");
   return (
